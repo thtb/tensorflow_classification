59f0d44a22b615bdd983f16c2b6256c476e49cdc
thtb <mufunan@gmail.com>
2017-12-27 10:33:07 +0800

    add rnn

--- classifier.py
+++ classifier.py
@@ -9,16 +9,17 @@ import sys
 import numpy as np
 
 import tensorflow as tf
+from tensorflow.contrib import rnn
 
 import inputs
 
+# 共有参数
+tf.flags.DEFINE_string("model_type", None, "mode type: fasttext, rnn, cnn")
 tf.flags.DEFINE_string("train_records", None,
-                       "Training file pattern for TFRecords, can use wildcards")
+                       "Training file pattern for TFRecords")
 tf.flags.DEFINE_string("eval_records", None,
-                       "Evaluation file pattern for TFRecords, can use wildcards")
+                       "Evaluation file pattern for TFRecords")
 tf.flags.DEFINE_string("gold_label_file", None, "")
-tf.flags.DEFINE_string("predict_records", None,
-                       "File pattern for TFRecords to predict, can use wildcards")
 tf.flags.DEFINE_string("label_file", None, "File containing output labels")
 tf.flags.DEFINE_string("vocab_file", None, "Vocabulary file, one word per line")
 tf.flags.DEFINE_integer("vocab_size", None, "Number of words in vocabulary")
@@ -31,46 +32,66 @@ tf.flags.DEFINE_string("export_dir", None, "Directory to store savedmodel")
 tf.flags.DEFINE_integer("embedding_dimension", 15,
                         "Dimension of word embedding")
 tf.flags.DEFINE_integer("attention_dimension", 16, "Dimension of attention")
+tf.flags.DEFINE_string("use_attention", False, "Whether to use attention")
+tf.flags.DEFINE_integer("rnn_dimension", 16, "Dimension of rnn")
 
 tf.flags.DEFINE_float("learning_rate", 0.001, "Learning rate for training")
 tf.flags.DEFINE_float("clip_gradient", 5.0, "Clip gradient norm to this ratio")
-tf.flags.DEFINE_integer("batch_size", 128, "Training minibatch size")
+tf.flags.DEFINE_integer("batch_size", 128, "Training mini batch size")
 tf.flags.DEFINE_integer("num_epochs", None, "Number of training data epochs")
 tf.flags.DEFINE_integer("checkpoint_steps", 1000,
                         "Steps between saving checkpoints")
 tf.flags.DEFINE_integer("num_threads", 1, "Number of reader threads")
-tf.flags.DEFINE_boolean("log_device_placement", False,
-                        "log where ops are located")
 tf.flags.DEFINE_float("per_process_gpu_memory_fraction", 0.5,
                       "per_process_gpu_memory_fraction")
 tf.flags.DEFINE_boolean("debug", False, "Debug")
 FLAGS = tf.flags.FLAGS
 
 
-def fasttext_estimator(model_dir):
+def attention(text_embedding, attention_dim):
+    attention_matrix \
+        = tf.contrib.layers.fully_connected(inputs=text_embedding,
+                                            num_outputs=attention_dim,
+                                            activation_fn=tf.nn.tanh)
+    attention_vector = tf.Variable(
+        tf.random_uniform([FLAGS.attention_dimension],
+                          -1.0 / FLAGS.attention_dimension,
+                          1.0 / FLAGS.attention_dimension))
+
+    alpha = tf.nn.softmax(
+        tf.reduce_sum(tf.multiply(attention_matrix, attention_vector), axis=2,
+                      keep_dims=True), dim=1)
+    attention_embedding = tf.reduce_sum(tf.multiply(text_embedding, alpha),
+                                        axis=1, keep_dims=False)
+    return attention_embedding
+
+
+def rnn_estimator(model_dir):
     params = {
         "learning_rate": FLAGS.learning_rate,
     }
 
     def model_fn(features, labels, mode, params):
         num_classes = len(open(FLAGS.label_file).readlines())
-        text_ids = features
-        text_embedding_w = tf.Variable(tf.random_uniform(
+        word_ids = features
+        word_embedding_table = tf.Variable(tf.random_uniform(
             [FLAGS.vocab_size + FLAGS.num_oov_vocab_buckets,
              FLAGS.embedding_dimension],
             -1.0 / FLAGS.embedding_dimension, 1.0 / FLAGS.embedding_dimension))
-        # text_embedding = tf.nn.embedding_lookup(text_embedding_w, text_ids)
-        # attention_matrix = tf.contrib.layers.fully_connected(inputs=text_embedding, num_outputs=FLAGS.attention_dimension, activation_fn=tf.nn.tanh)
-        # attention_vector = tf.Variable(tf.random_uniform([FLAGS.attention_dimension], -1.0 / FLAGS.attention_dimension, 1.0 / FLAGS.attention_dimension))
-        ##attention_vector = tf.Variable(tf.zeros([FLAGS.attention_dimension]))
-        # alpha = tf.nn.softmax(tf.reduce_sum(tf.multiply(attention_matrix, attention_vector), axis=2, keep_dims=True), dim=1)
-        # attention_embedding = tf.reduce_sum(tf.multiply(text_embedding, alpha), axis=1, keep_dims=False)
-        # input_layer = attention_embedding
-
-        text_embedding = tf.reduce_mean(tf.nn.embedding_lookup(
-            text_embedding_w, text_ids), axis=-2)
-        input_layer = text_embedding
-
+        word_embeddings = tf.nn.embedding_lookup(word_embedding_table, word_ids)
+        print(word_embeddings.shape)
+        rnn_fw_cell = rnn.BasicLSTMCell(FLAGS.rnn_dimension)
+        rnn_bw_cell = rnn.BasicLSTMCell(FLAGS.rnn_dimension)
+        outputs, _ = tf.nn.bidirectional_dynamic_rnn(rnn_fw_cell, rnn_bw_cell,
+                                                  word_embeddings,
+                                                  dtype=tf.float32)
+        text_embedding = tf.concat(outputs, 2)
+        print(text_embedding.shape)
+        if FLAGS.use_attention:
+            input_layer = attention(text_embedding, FLAGS.attention_dimension)
+        else:
+            input_layer = tf.reduce_mean(text_embedding, axis=-2)
+        print(input_layer.shape)
         logits = tf.contrib.layers.fully_connected(
             inputs=input_layer, num_outputs=num_classes,
             biases_initializer=None,
@@ -85,30 +106,74 @@ def fasttext_estimator(model_dir):
                                                                logits=logits))
             labels = tf.squeeze(labels, -1)
             opt = tf.train.AdamOptimizer(params["learning_rate"])
-            opt_sgd = tf.train.GradientDescentOptimizer(learning_rate=0.01)
             train_op = opt.minimize(loss,
-                                        global_step=tf.train.get_global_step())
+                                    global_step=tf.train.get_global_step())
             metrics = {
                 "accuracy": tf.metrics.accuracy(labels, predictions)
             }
         exports = {}
-        # if FLAGS.export_dir:
-        probs = tf.nn.softmax(logits)
-        # exports["probs"] = tf.estimator.export.ClassificationOutput(
-        #     scores=probs)
-        # exports["embedding"] = tf.estimator.export.RegressionOutput(
-        #     value=text_embedding)
-        # exports[
-        #     tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY] = \
-        #     tf.estimator.export.ClassificationOutput(scores=probs)
+        probability = tf.nn.softmax(logits)
         return tf.estimator.EstimatorSpec(
-            mode, predictions=probs, loss=loss, train_op=train_op,
+            mode, predictions=probability, loss=loss, train_op=train_op,
             eval_metric_ops=metrics, export_outputs=exports)
 
     gpu_option = tf.GPUOptions(
         per_process_gpu_memory_fraction=FLAGS.per_process_gpu_memory_fraction)
     session_config = tf.ConfigProto(
-        log_device_placement=FLAGS.log_device_placement,
+        gpu_options=gpu_option)
+    config = tf.contrib.learn.RunConfig(
+        session_config=session_config)
+    return tf.estimator.Estimator(model_fn=model_fn, model_dir=model_dir,
+                                  params=params, config=config)
+
+
+def fasttext_estimator(model_dir):
+    params = {
+        "learning_rate": FLAGS.learning_rate,
+    }
+
+    def model_fn(features, labels, mode, params):
+        num_classes = len(open(FLAGS.label_file).readlines())
+        text_ids = features
+        text_embedding_w = tf.Variable(tf.random_uniform(
+            [FLAGS.vocab_size + FLAGS.num_oov_vocab_buckets,
+             FLAGS.embedding_dimension],
+            -1.0 / FLAGS.embedding_dimension, 1.0 / FLAGS.embedding_dimension))
+        print(tf.nn.embedding_lookup(
+            text_embedding_w, text_ids).shape)
+        text_embedding = tf.reduce_mean(tf.nn.embedding_lookup(
+            text_embedding_w, text_ids), axis=-2)
+        input_layer = text_embedding
+        print(input_layer.shape)
+        sys.stdout.flush()
+        logits = tf.contrib.layers.fully_connected(
+            inputs=input_layer, num_outputs=num_classes,
+            biases_initializer=None,
+            activation_fn=None)
+        logits = tf.expand_dims(logits, -2)
+        predictions = tf.argmax(logits, axis=-1)
+        loss, train_op = None, None
+        metrics = {}
+        if mode != tf.estimator.ModeKeys.PREDICT:
+            loss = tf.reduce_mean(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels,
+                                                               logits=logits))
+            labels = tf.squeeze(labels, -1)
+            opt = tf.train.AdamOptimizer(params["learning_rate"])
+            train_op = opt.minimize(loss,
+                                    global_step=tf.train.get_global_step())
+            metrics = {
+                "accuracy": tf.metrics.accuracy(labels, predictions)
+            }
+        exports = {}
+        probability = tf.nn.softmax(logits)
+        return tf.estimator.EstimatorSpec(
+            mode, predictions=probability, loss=loss, train_op=train_op,
+            eval_metric_ops=metrics, export_outputs=exports)
+
+    gpu_option = tf.GPUOptions(
+        per_process_gpu_memory_fraction=FLAGS.per_process_gpu_memory_fraction)
+    session_config = tf.ConfigProto(
         gpu_options=gpu_option)
     config = tf.contrib.learn.RunConfig(
         session_config=session_config)
@@ -131,34 +196,28 @@ def calculate_prf(gold_count, predict_count, right_count):
 def calculate_performance(predict_probability, gold, num_class, other_class,
                           threshold, debug_file="label_debug.txt"):
     debug_f = None
-    if debug_file != None:
+    if not debug_file:
         debug_f = open(debug_file, "w")
     confusion_matrix = np.zeros((num_class, num_class), dtype=np.int64)
     # filter max predict prob less than threshold
     filtered_confusion_matrix = np.zeros((num_class, num_class), dtype=np.int64)
-    gold_label_line_count = len(gold)
     line_count = 0
     for predict in predict_probability:
         gold_label = gold[line_count]
         predict_np = np.array(predict)
         predict_label = predict_np.argmax()
-        if debug_f != None:
+        if not debug_f:
             debug_f.write("%d\t%d\n" % (predict_label, gold_label))
         confusion_matrix[gold_label][predict_label] += 1
         if predict_np.max() > threshold:
             filtered_confusion_matrix[gold_label][predict_label] += 1
         line_count += 1
-    print(gold_label_line_count, line_count)
-    # erase 'other' category count
-    print(filtered_confusion_matrix)
     gold_count_category = filtered_confusion_matrix.sum(axis=1)
     predict_count_category = filtered_confusion_matrix.sum(axis=0)
+    # erase 'other' category count
     for other_id in other_class:
         gold_count_category[other_id] = 0
         predict_count_category[other_id] = 0
-    print(gold_count_category)
-    print
-    print(predict_count_category)
     gold_count = 0
     predict_count = 0
     right_count = 0
@@ -186,28 +245,28 @@ def train():
     print(other_labels)
     sys.stdout.write("init")
     hook = tf.train.ProfilerHook(save_steps=100, output_dir='./timeline')
-    estimator = fasttext_estimator(FLAGS.model_dir)
+    if FLAGS.model_type == "rnn":
+        estimator = rnn_estimator(FLAGS.model_dir)
+        input_fn = inputs.dataset_nn_input_fn
+    else:
+        estimator = fasttext_estimator(FLAGS.model_dir)
+        input_fn = inputs.fasttext_input_fn
     for i in xrange(0, FLAGS.num_epochs):
-        train_input = inputs.fasttext_input_fn(tf.estimator.ModeKeys.TRAIN,
+        train_input = input_fn(tf.estimator.ModeKeys.TRAIN,
                                                FLAGS.train_records,
                                                FLAGS.batch_size)
         sys.stdout.write("start training epoch %d\n" % i)
         # estimator.train(input_fn=train_input, steps=10000, hooks=None)
-        estimator.train(input_fn=train_input, hooks=None)
+        estimator.train(input_fn=train_input, hooks=[hook])
         sys.stdout.write("start evaluate at epoch %d\n" % i)
-        predict_input = inputs.fasttext_input_fn(tf.estimator.ModeKeys.PREDICT,
+        predict_input = input_fn(tf.estimator.ModeKeys.PREDICT,
                                                  FLAGS.eval_records, 1)
         predict = estimator.predict(input_fn=predict_input, hooks=None)
-        eval_input = inputs.fasttext_input_fn(tf.estimator.ModeKeys.PREDICT,
-                                                 FLAGS.eval_records, 1)
-        estimator.evaluate(input_fn=eval_input, hooks=None)
-        train_eval_input = inputs.fasttext_input_fn(tf.estimator.ModeKeys.PREDICT,
-                                                 FLAGS.train_records, 1)
-        estimator.evaluate(input_fn=train_eval_input, hooks=None)
         sys.stdout.write("eval done\n")
         sys.stdout.write("epoch %d precision recall f_score:\n" % i)
-        sys.stdout.write("%f\t%f\t%f\t" % (calculate_performance(predict, gold_label, len(labels),
-                                    other_labels, 0)))
+        sys.stdout.write("%f\t%f\t%f\n" % (
+            calculate_performance(predict, gold_label, len(labels),
+                                  other_labels, 0)))
         sys.stdout.flush()
 
 
--- inputs.py
+++ inputs.py
@@ -1,58 +1,54 @@
 import tensorflow as tf
 
 
-def nn_input_fn(mode,
+def dataset_nn_input_fn(mode,
                 input_file,
                 batch_size,
                 num_epochs=1):
-    if num_epochs <= 0:
-        num_epochs = 1
-
     def input_fn():
         def parser(record):
-            keys_to_features = {
+            keys_to_sample = {
                 "features": tf.VarLenFeature(dtype=tf.int64),
                 "label": tf.FixedLenFeature(shape=(1,), dtype=tf.int64,
                                             default_value=None),
                 "real_len": tf.FixedLenFeature(shape=(1,), dtype=tf.int64,
                                                default_value=None)
             }
-            parsed = tf.parse_single_example(record, keys_to_features)
-            return tf.sparse_tensor_to_dense(parsed["features"],
-                                             default_value=" "), parsed["label"]
+            parsed = tf.parse_single_example(record, keys_to_sample)
+            features = tf.sparse_tensor_to_dense(parsed["features"],
+                                                 default_value=" ")
+            return features, parsed["label"], parsed["real_len"]
 
         dataset = tf.data.TFRecordDataset(input_file)
         dataset = dataset.map(parser)
-        dataset = dataset.shuffle(buffer_size=1000000)
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            dataset = dataset.shuffle(buffer_size=100000)
         dataset = dataset.batch(batch_size)
         dataset = dataset.repeat(num_epochs)
         iterator = dataset.make_one_shot_iterator()
 
-        features, labels = iterator.get_next()
+        features, labels, real_len = iterator.get_next()
         return features, labels
 
     return input_fn
 
 
-def fasttext_input_fn(mode, input_file, batch_size, num_epochs=None,
-                      num_threads=3):
-    if num_epochs <= 0:
-        num_epochs = 1
-
+def fasttext_input_fn(mode, input_file, batch_size, num_epochs=1,
+                      num_threads=2):
     def input_fn():
-        keys_to_sample = dict()
-        keys_to_sample["features"] = tf.VarLenFeature(dtype=tf.int64)
-        keys_to_sample["label"] = tf.FixedLenFeature(shape=(1,),
-                                                     dtype=tf.int64,
-                                                     default_value=None)
-
+        keys_to_sample = {
+            "features": tf.VarLenFeature(dtype=tf.int64),
+            "label": tf.FixedLenFeature(shape=(1,),
+                                        dtype=tf.int64,
+                                        default_value=None)
+        }
         randomize_input = False
         if mode == tf.estimator.ModeKeys.TRAIN:
             randomize_input = True
         sample = tf.contrib.learn.read_batch_features(
             input_file, batch_size, keys_to_sample, tf.TFRecordReader,
             num_epochs=num_epochs, reader_num_threads=num_threads,
-            queue_capacity=10000, randomize_input=randomize_input)
+            queue_capacity=100000, randomize_input=randomize_input)
         features = tf.sparse_tensor_to_dense(sample["features"])
         label = sample["label"]
         return features, label
